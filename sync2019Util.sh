8ad535a987c02c4946898dd242d112f2e689933b
鞠爱华 <600852@syswin.com>
2019-07-31 10:19:18 +0800

    sync script optimize

--- sync2019Util.sh
+++ sync2019Util.sh
@@ -4,7 +4,7 @@ project_git=$1
 project_name=$2
 branch_name=$3
 commit_info=$4
-project_wordkir=sync-$2
+project_workir=sync-$2
 project_path=$(cd `dirname $0`; pwd)
 echo "server 2019 git location: $project_git"
 echo "server 2019 project name: $project_name"
@@ -12,9 +12,9 @@ echo "server 2019 branch name: $branch_name"
 echo "server 2019 branch commit info: $commit_info"
 
 cd ~
-rm -rf $project_wordkir
-mkdir $project_wordkir
-cd $project_wordkir
+rm -rf $project_workir
+mkdir $project_workir
+cd $project_workir
 git clone $project_git
 cd $project_name
 git pull origin $branch_name
@@ -24,11 +24,13 @@ rm -rf ./.gitignore
 
 cp -r $project_path/* ./
 cp $project_path/.gitignore ./
+rm -rf ./sync*.sh
 
 git add ./*
 echo "ready to commit with : $commit_info "
 git commit -m "$commit_info"
 
-#git push origin $branch_name
-#rm -rf ~/sync-dircetory-gateway
-#cd $project_path
+git push origin $branch_name
+
+cd $project_path
+#rm -rf ~/$project_workir
